953dce60bcdd5f22b4fd7eccc7b005ba1dd93e6a
alansouza7 <alansouzauk@gmail.com>
2023-11-06 11:22:34 +0000

    text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,9 +12,7 @@ const About = () => {
             <Link to="/" className='btn btn-secondary mt-4 text-2xl font-bold'> RMS</Link>
        
         </div>
-      <p className='mt-8 max-w-lg m-auto'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque consectetur tempora similique
-             facere est quasi quidem facilis blanditiis, qui incidunt, saepe quo
-             ? Quae adipisci quam, nam laudantium provident id fugiat?</p>
+      <p className='mt-8 max-w-lg m-auto'>At our innovative platform, we are transforming the way you rent products, ushering in a new era of convenience and accessibility. We believe in making the renting process not just a transaction, but an experience that enriches your life. We're changing the way we rent by putting you in control, offering a diverse range of products that cater to your unique needs and desires. Whether it's the latest tech gadgets, furniture for your living space, or outdoor equipment for your adventures, we're here to provide a seamless and user-friendly renting experience. Embrace a world where renting is a breeze, and where you have the power to adapt and enjoy the products you need, whenever you need them. Welcome to the future of renting, where choice, flexibility, and simplicity converge.</p>
     </div>
   )
 }
