484b5e61f06ffd7079454f287d9e87e4eec66ada
alansouza7 <alansouzauk@gmail.com>
2023-11-07 09:09:01 +0000

    text spacing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
             <Link to="/" className='btn btn-secondary mt-4 text-2xl font-bold'> RMS</Link>
        
         </div>
-      <p className='mt-8 max-w-lg m-auto'>At our innovative platform, we are transforming the way you rent products, ushering in a new era of convenience and accessibility. We believe in making the renting process not just a transaction, but an experience that enriches your life. We're changing the way we rent by putting you in control, offering a diverse range of products that cater to your unique needs and desires. Whether it's the latest tech gadgets, furniture for your living space, or outdoor equipment for your adventures, we're here to provide a seamless and user-friendly renting experience. Embrace a world where renting is a breeze, and where you have the power to adapt and enjoy the products you need, whenever you need them. Welcome to the future of renting, where choice, flexibility, and simplicity converge.</p>
+      <p className='mt-8 max-w-lg m-auto'>We're changing the way we rent by putting you in control, offering a diverse range of products that cater to your unique needs and desires.<br></br><br></br> Whether it's the latest tech gadgets, furniture for your living space, or outdoor equipment for your adventures, we're here to provide a seamless and user-friendly renting experience.<br></br><br></br> Embrace a world where renting is a breeze, and where you have the power to adapt and enjoy the products you need, whenever you need them. Welcome to the future of renting, where choice, flexibility, and simplicity converge.</p>
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
         </div>
             <div>
                 <h1 className="text-5xl lg:text-7xl font-bold">We are changing the way people rent</h1>
-                <p className="py-6 ">At our innovative platform, we are transforming the way you rent products, ushering in a new era of convenience and accessibility. We believe in making the renting process not just a transaction, but an experience that enriches your life. We're changing the way we rent by putting you in control, offering a diverse range of products that cater to your unique needs and desires. Whether it's the latest tech gadgets, furniture for your living space, or outdoor equipment for your adventures, we're here to provide a seamless and user-friendly renting experience. Embrace a world where renting is a breeze, and where you have the power to adapt and enjoy the products you need, whenever you need them. Welcome to the future of renting, where choice, flexibility, and simplicity converge.</p>
+                <p className="py-6 ">We're changing the way we rent by putting you in control, offering a diverse range of products that cater to your unique needs and desires.<br></br><br></br> Whether it's the latest tech gadgets, furniture for your living space, or outdoor equipment for your adventures, we're here to provide a seamless and user-friendly renting experience.<br></br><br></br> Embrace a world where renting is a breeze, and where you have the power to adapt and enjoy the products you need, whenever you need them. Welcome to the future of renting, where choice, flexibility, and simplicity converge.</p>
                 <Link to="/products" className="btn btn-primary font-bold">our products</Link>
             </div>
     </div>
